309e181ffe3adea01e20d3cff05c5b9321e53228
Akash Desai <kash650@gmail.com>
2018-04-19 13:19:35 -0700

    moved packages

---
+++ BoundlessKitExample/boundlesskit/src/main/java/boundless/kit/integration/dashboard/codeless/CodelessReward.java
@@ -0,0 +1,5 @@
+package boundless.kit.integration.dashboard.codeless;
+
+public class CodelessReward {
+
+}
--- BoundlessKitExample/boundlesskit/src/main/java/boundless/kit/integration/dashboard/codeless/rewards/CodelessReward.java
+++
@@ -1,5 +0,0 @@
-package boundless.kit.integration.dashboard.codeless.rewards;
-
-public class CodelessReward {
-
-}
