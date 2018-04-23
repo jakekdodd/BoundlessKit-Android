660167bfa8d5b6f2478005cda9d811f8b59067a1
Akash Desai <kash650@gmail.com>
2018-04-23 10:49:34 -0700

    using Path for animation

--- BoundlessKitExample/boundlesskit/src/main/java/boundless/kit/rewards/animation/attention/ShimmyAnimator.java
+++ BoundlessKitExample/boundlesskit/src/main/java/boundless/kit/rewards/animation/attention/ShimmyAnimator.java
@@ -24,6 +24,7 @@
 
 package boundless.kit.rewards.animation.attention;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.graphics.Path;
 import android.view.View;
@@ -34,7 +35,7 @@ public class ShimmyAnimator extends BaseViewAnimator {
 
     public static class Builder {
         private int _count = 2;
-        private long _duration = 3000;
+        private long _duration = 5000;
         private long _translation = 30;
         private long _speed = 3;
 
@@ -57,9 +58,9 @@ public class ShimmyAnimator extends BaseViewAnimator {
 
     private int count;
     private float translation;
-    private float speed;
+    private long speed;
 
-    public ShimmyAnimator(int count, long duration, float translation, float speed) {
+    public ShimmyAnimator(int count, long duration, float translation, long speed) {
         super();
         this.count = count;
         setDuration(duration);
@@ -69,58 +70,18 @@ public class ShimmyAnimator extends BaseViewAnimator {
 
     @Override
     public void prepare(View target) {
-        float width = target.getWidth();
-        float one = (float) (width / 100.0);
-//        getAnimatorAgent().playTogether(
-//                ObjectAnimator.ofFloat(target, "translationX", 0 * one, -25 * one, 20 * one, -15 * one, 10 * one, -5 * one, 0 * one, 0)
-//
-////                Glider.glide(Skill.ElasticEaseIn, 1200, ObjectAnimator.ofFloat(target, "translationY", 0, 100));
-////
-////                ObjectAnimator.ofFloat(target, "translationX", floats.toArray(new Float[floats.size()]))
-//
-//        );
-
-//        ArrayList<Animator> animators = new ArrayList<>();
-//        if (count > 0) {
-//            animators.add(ObjectAnimator.ofFloat(target, "translationX", 0, translation, -translation));
-//            for(int i = 1; i < count; i++) {
-//                animators.add(ObjectAnimator.ofFloat(target, "translationX", -translation, translation, -translation));
-//            }
-//            animators.add(ObjectAnimator.ofFloat(target, "translationX", -translation, 0));
-//        }
-//        getAnimatorAgent().playSequentially(animators);
-//        AccelerateDecelerateInterpolator interpolator = new AccelerateDecelerateInterpolator();
-//        interpolator.getInterpolation(0.1f);
-//        getAnimatorAgent().setInterpolator(new AccelerateDecelerateInterpolator());
-
-//        getAnimatorAgent().play(Glider.glide(Skill.CubicEaseInOut, getDuration(), ObjectAnimator.ofFloat(target, "translationX", 1f, 1.5f)));
-//        getAnimatorAgent().playSequentially(
-//                Glider.glide(Skill.ElasticEaseIn, getDuration()/2, ObjectAnimator.ofFloat(target, "translationX", translation))
-////                Glider.glide(Skill.ElasticEaseOut, getDuration()/2, ObjectAnimator.ofFloat(target, "translationX", -translation))
-//        );
-
-
-//        getAnimatorAgent().setInterpolator(PathInterpolatorCompat.create(0, 0, translation, 0));
-
         float x = target.getX();
         float y = target.getY();
         Path path = new Path();
         path.moveTo(x, y);
+        float xMove = target.getWidth() * (translation/100);
         for(int i = 0; i < count; i++) {
-            path.lineTo(x + translation, y);
-            path.lineTo(x - translation, y);
+            path.lineTo(x + xMove, y);
+            path.lineTo(x - xMove, y);
         }
         path.lineTo(x, y);
-        getAnimatorAgent().play(ObjectAnimator.ofFloat(target, View.X, View.Y, path));
-
-
-//        ArrayList<ArrayList<Float>> floats = new ArrayList<>();
-//        floats.add(new ArrayList<Float>(){{add(0f);}});
-//        for(int i = 0; i < count; i++) {
-//            floats.add(new ArrayList<Float>(){{add(translation);}});
-//            floats.add(new ArrayList<Float>(){{add(-translation);}});
-//        }
-//        floats.add(new ArrayList<Float>(){{add(0f);}});
-//        getAnimatorAgent().play(ObjectAnimator.ofMultiFloat(target, "translationX", floats.stream().map(a -> a.stream().map(f -> f.floatValue())).toArray()));
+        Animator animator = ObjectAnimator.ofFloat(target, View.X, View.Y, path);
+        setDuration(getDuration() / speed);
+        getAnimatorAgent().play(animator);
     }
 }
